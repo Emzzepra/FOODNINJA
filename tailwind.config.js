92e263eb40dde0df2ab7091fe735884fd83fb3d3
Emmanuel Ajala <127834217+Emzzepra@users.noreply.github.com>
2023-08-19 14:28:58 +0100

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html","./src/**/*.{html,js}"],
+  content: ["./index.html", "./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#FF6363",
+        secondary: {
+          100: "#E2E2D5",
+          200: "#888883",
+        },
+      },
+      fontFamily: {
+        body: ["Nunito"],
+      },
+    },
   },
+  extend: {},
   plugins: [],
-}
+};
